2ec8fcacd656aa50ffe249f4681636044ad87364
gregjhansell97 <gregjhansell@gmail.com>
2019-10-19 00:21:04 -0400

    interface/implementation created

--- channel.cpp
+++ channel.cpp
@@ -2,6 +2,11 @@
 
 namespace hmap {
 
+
+void Channel::write(msg::Header* h) {
+    this->write(static_cast<char*>(static_cast<void*>(h)), h->size);
+}
+
 size_t Channel::read(msg::Header* h) {
     this->read(static_cast<char*>(static_cast<void*>(h)), sizeof(msg::Header));
     // look at header and if its on ringbuffer already suck in rest of message
